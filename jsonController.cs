74a20f99783c5fc9ef637769d40e1ac7393edfe8
abskydev <Ghjpl125711pl>
2022-01-30 18:32:16 +0100

    created a json response controller

---
+++ jsonController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft;
+using Newtonsoft.Json;
+
+namespace currencyPriceChecker
+{
+    class jsonController
+    {
+        public Response getValFromJson(string json)
+        {
+            Response response = JsonConvert.DeserializeObject<Response>(json);
+            return response;
+        }
+    }
+    public class Response
+    {
+        public string table { get; set; }
+        public string currency { get; set; }
+        public string code { get; set; }
+        public IList<string> rates { get; set; }
+    }
+}
