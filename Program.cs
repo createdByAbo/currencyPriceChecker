bc71bfa2c9b4ac236e5e7e635f74e8f3d3bc16ac
abskydev <Ghjpl125711pl>
2022-01-30 17:32:06 +0100

    start using OOP

--- Program.cs
+++ Program.cs
@@ -8,19 +8,7 @@ namespace currencyPriceChecker
     {
         static void Main(string[] args)
         {
-            getCurrencyPrice(Console.ReadLine());
-        }
-        static void getCurrencyPrice(string currency)
-        {
-            WebRequest request = WebRequest.Create($"http://api.nbp.pl/api/exchangerates/rates/a/{currency}/last/1/?format=json");
-            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-            Stream dataStream = response.GetResponseStream();
-            StreamReader reader = new StreamReader(dataStream);
-            string responseFromServer = reader.ReadToEnd();
-            Console.WriteLine(responseFromServer);
-            reader.Close();
-            dataStream.Close();
-            response.Close();
+            httpRequester.request(Console.ReadLine());
         }
     }
 }
---
+++ httpRequester.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace currencyPriceChecker
+{
+    class httpRequester
+    {
+        public static void request(string currency)
+        {
+            WebRequest request = WebRequest.Create($"http://api.nbp.pl/api/exchangerates/rates/a/{currency}/last/1/?format=json");
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            string responseFromServer = reader.ReadToEnd();
+            Console.WriteLine(responseFromServer);
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+        }
+    }
+}
