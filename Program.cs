caf0b0f394c3db248d183a038275cf59ebffeecf
abskydev <Ghjpl125711pl>
2022-01-30 17:50:08 +0100

    make multi time check functionality

--- Program.cs
+++ Program.cs
@@ -6,7 +6,29 @@ namespace currencyPriceChecker
     {
         static void Main(string[] args)
         {
-            httpRequester.getRequest(Console.ReadLine());
+            bool stop = false;
+            while(!stop)
+            {
+                Console.WriteLine("Type currency code");
+                httpRequester.getRequest(Console.ReadLine().ToLower());
+                Console.WriteLine("you want to check next currency? /c to continue /s to stop");
+                string userInput = Console.ReadLine();
+                if (userInput == "/c")
+                {
+                    continue;
+                }else if (userInput == "/s")
+                {
+                    stop = true;
+                    break;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Beep();
+                    Console.WriteLine("error");
+                    Console.ResetColor();
+                }
+            }
         }
     }
 }
